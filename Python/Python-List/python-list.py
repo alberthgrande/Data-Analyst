30e5f547a5af10f16696114fe0196078fe31dd29
alberthgrande <alberthgrande98@gmail.com>
2026-05-25 10:17:31 +0800

    Added Python List

--- Python/Python-List/python-list.py
+++ Python/Python-List/python-list.py
@@ -1,2 +1,30 @@
 thislist = ["apple", "banana", "cherry"]
 print(thislist)
+
+# Allow duplicate
+allowduplicate = ["apple", "banana", "cherry", "apple"]
+print(allowduplicate)
+
+# List Length
+print(len(allowduplicate))
+
+# List Items - Data Types
+list1 = ["apple", "banana", "cherry"]
+list2 = [1, 2, 3, 4, 5]
+list3 = [True, False, False]
+
+print(list1)
+print(list2)
+print(list3)
+
+arr_person_info = ["Alberth", 27, "male", True]
+print(arr_person_info)
+
+# type()
+print(type(arr_person_info))
+
+# The list() Constructor
+newlist = list(("alberth", 27, "male", True))
+print(newlist)
+
+
