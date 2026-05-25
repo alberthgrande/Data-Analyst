cbfa27735c86bcbc5d08f71f779bc1f1cc2b85eb
alberthgrande <alberthgrande98@gmail.com>
2026-05-25 22:15:29 +0800

    Modified Added Any Iterable

--- Python/Python-List/python-add-list.py
+++ Python/Python-List/python-add-list.py
@@ -1,5 +1,19 @@
+# Append Items at the end list 'append()'
 fruitlist = ["apple", "banana", "cherry"]
 fruitlist.append("orange")
 print(fruitlist)
 
+# Insert Items at specified index 'insert(index,value)'
+fruitlist.insert(4, "watermelon")
+print(fruitlist)
 
+# Extend List append other list to another 'extends()'
+fruitlist2 = ["melon", "lemon", "papaya"]
+fruitlist2.extend(fruitlist)
+print(fruitlist2)
+
+# Add Any Iterable 'extend()''
+thislist = ["apple","banana","cherry"]
+thistuple = ("kiwi","orange")
+thislist.extend(thistuple)
+print(thislist)
