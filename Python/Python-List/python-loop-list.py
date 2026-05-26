73ed828b7c60a577399b23095f28df12de5fb5dc
alberthgrande <alberthgrande98@gmail.com>
2026-05-26 12:11:50 +0800

    Added Python Loop List

---
+++ Python/Python-List/python-loop-list.py
@@ -0,0 +1,72 @@
+# Loop Through a List using "for"
+fruitlist = ["apple","banana","cherry"]
+for fruits in fruitlist:
+    print(fruits)
+
+# Looping Through a String
+print("\n")
+for banana in "banana":
+    print(banana)
+
+# The break Statement
+print("\n")
+for fruits in fruitlist:
+    #print(fruits)
+    if fruits == "banana":
+        #print(fruits)
+        break
+    print(fruits)
+
+# The continue Statement
+print("\n")
+for fruits in fruitlist:
+    if fruits == "banana":
+        continue
+    print(fruits)
+
+# The range() Function
+print("\n")
+for x in range(6):
+    print(x)
+    
+# specify parameters start and end "range(start,end)"
+print("\n")
+for x in range(2,6):
+    print(x)
+
+# increment sequence by adding third parameter "range(start,end,increment)"
+print("\n")
+for x in range(2,30,3):
+    print(x)
+
+# Else in For Loop
+print("\n")
+for x in range(6):
+    print(x)
+else:
+    print("Finally finished!")
+
+# if use the break the else will not be executed
+print("\n")
+for x in range(x):
+    if x == 3: break
+    print(x)
+else:
+    print("Finally finished!")
+
+# Nested Loops
+print("\n")
+adj = ["red","big","tasty"]
+fruits = ["apple","watermelon","orange"]
+
+for x in adj:
+   # print(x)
+    for y in fruits:
+        print(x , y)
+
+# The pass Statement
+print("\n")
+for x in [0, 1, 2]:
+    # print(x)
+    pass
+
