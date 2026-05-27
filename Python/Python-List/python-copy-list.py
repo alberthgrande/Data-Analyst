730bc571f83d264c2423b2e91123bc1d9065f9c3
alberthgrande <alberthgrande98@gmail.com>
2026-05-27 21:42:44 +0800

    Added Python Copy List

---
+++ Python/Python-List/python-copy-list.py
@@ -0,0 +1,19 @@
+# Use the copy() method
+thislist = ["apple","banana","cherry"]
+usecopy = thislist.copy()
+print(usecopy)
+
+#Use the list() method
+print("\n")
+uselist = list(thislist)
+print(uselist)
+
+# Use the slice Operator
+print("\n")
+useslice = thislist[:]
+print(useslice)
+
+
+
+
+
