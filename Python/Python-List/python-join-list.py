b4d21ba16a02057f3902fe0a7b0337d2c4df34e2
alberthgrande <alberthgrande98@gmail.com>
2026-05-27 23:24:57 +0800

    Added Python Join List

---
+++ Python/Python-List/python-join-list.py
@@ -0,0 +1,20 @@
+# Join Two Lists using "+" operator
+list1 = ["apple", "banana", "cherry"]
+list2 = [1, 2, 3]
+list3 = list1 + list2
+print(list3)
+
+# using append()
+print("\n")
+for x in list2:
+    pass
+    #list1.append(x)    
+#print(list1)
+
+# using extend
+print("\n")
+list1.extend(list2)
+print(list1)
+
+
+
