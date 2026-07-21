44bfe2d28ce254d8ccf166c0edff0220edba9e06
alberthgrande <alberthgrande98@gmail.com>
2026-07-21 16:54:00 +0800

    Modified added Practice Problem 4, 5, 6, and 7

--- SQL/Practice Problem/practice2.sql
+++ SQL/Practice Problem/practice2.sql
@@ -38,3 +38,54 @@ FROM
 WHERE
 	job_schedule_type <> 'Part-time';
 
+
+
+-- Practice Problem 4
+SELECT
+	job_id,
+    job_title_short, 
+    job_location, 
+    job_via,
+	salary_year_avg
+FROM
+	job_postings_fact
+WHERE
+	job_via <> 'via LinkedIn';
+
+-- Practice Problem 5
+SELECT 
+	job_id
+    job_title_short,
+    job_location,
+    job_via, 
+	salary_year_avg
+FROM
+	job_postings_fact
+WHERE
+	salary_year_avg >= 65000;
+    
+    
+-- Practice Problem 6
+SELECT
+	job_id, 
+    job_title_short,
+	job_location,
+	job_via, 
+	salary_year_avg
+FROM
+	job_postings_fact
+WHERE
+	salary_year_avg >= 55000;
+    
+    
+-- Practice Problem 7
+SELECT
+	job_id, 
+    job_title_short, 
+    job_location, 
+    job_via, 
+    salary_year_avg
+FROM
+	job_postings_fact
+WHERE
+	salary_year_avg <= 110000;
